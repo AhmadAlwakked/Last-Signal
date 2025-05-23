4326f6d26924f46415f71166bfbd04e8858ecadf
mus-arch <mus14052007@outlook.com>
2025-05-23 12:46:09 +0200

    Last Signal (pre-pre-code)

--- Last-Signal/Assets/Codes/TowerRotator.cs
+++ Last-Signal/Assets/Codes/TowerRotator.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class TowerRotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public float speed;
 
     // Update is called once per frame
     void Update()
@@ -16,12 +13,40 @@ public class TowerRotator : MonoBehaviour
 
         target = FindNearestEnemy();
 
-        if(target !=null)
+        if (target != null)
         {
-            Vector3 direction =(target.position-transform.position).normalized;
+            Vector3 direction = (target.position - transform.position).normalized;
             Quaternion lookRotation = Quateratnion.LookRotation(new Vector3(direction.x, 0, direction.z));
-            transform.rotation = Quaternion
+            transform.rotation = Quaternion.Slerp(transfer.rotation, lookRotation, Time.deltaTime * speed)
+        }
+    }
+
+    protected Transform FindNearestEnemy()
+    {
+
+        GameObject[] enemies = Gameobject.FindGameObjectsWithTag("Enemy");
+        Transform closest = null;
+        float shortestDistance = Math.Infinity;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < shortestDistance && distance <= range)
+            {
+                shortestDistance = distance;
+                closest = enemy.transform;
+            }
         }
 
+        return closest;
     }
+
+    protected void OnDrawGizmosSelected()
+    {
+
+        OnDrawGizmosSelected.color = Color.bleu
+        OnDrawGizmosSelected(transform.postion, range);
+
+    }
+
 }

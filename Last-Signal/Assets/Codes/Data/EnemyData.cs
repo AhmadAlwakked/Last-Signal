4c5fa2a982e644b64714dc75a6584dae16ce6948
mus-arch <mus14052007@outlook.com>
2025-06-20 13:42:18 +0200

    no data

--- Last-Signal/Assets/Codes/Data/EnemyData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewEnemyData", menuName = "Tower Defense/Enemy Data")]
-public class EnemyData : ScriptableObject
-{
-    public string enemyName; // Naam van de vijand
-    public int rewardMaterials; // Materialen bij dood
-}
